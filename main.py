eb5f24bee87ec9cf638f5cc2ec094acb695564ba
lolsanti <55632755+lolsanti@users.noreply.github.com>
2023-02-27 22:58:02 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#new_value = 60
+#result = "less then 100" if new_value < 100 else "bigger then 100" #Не понятен вопрос
+#print(result)
+
+#new_value = 10000
+#result = 1 if new_value < 100 else 0 # можно использовать True, False вместо 1, 0 тк Tr=1,Fl=0
+#print(result)
+
+#new_value = 101
+#result = True if new_value < 100 else False
+#print(result)
+
+#my_str = 'кни'
+#if len(my_str) < 5:
+    #print(f"{my_str}ж")
+#else:
+    #print(my_str)
+
+#my_str = "кни"
+#if len(my_str) < 5:
+    #my_str += my_str[::-1]
+#print(my_str)
